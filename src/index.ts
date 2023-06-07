62bf6c675d42528c0a80eeec1c0756ee23c8fb4b
Taxin2012 <26491089+Taxin2012@users.noreply.github.com>
2023-06-08 02:01:12 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,7 @@ const imgbox = async (
         album_title?: string,
         album_id?: string,
         album_secret?: string,
-        use_album?: bool,
+        use_album?: Boolean,
         content_type?: ContentType,
         thumbnail_size?: ThumbnailSize,
         comments_enabled?: CommentEnabled,
